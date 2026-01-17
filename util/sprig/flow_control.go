9d9363ea5a438b9d536c2d5001a783d6272b08f4
Alex Sierputowski <interfaceas@monopoly>
2026-01-17 17:19:12 -0500

    WIP before creating cli branch

--- util/sprig/flow_control.go
+++
@@ -1,8 +0,0 @@
-package sprig
-
-import "errors"
-
-// fail is a function that always returns an error with the given message.
-func fail(msg string) (string, error) {
-	return "", errors.New(msg)
-}
